e1f27bbf1f767458a3912fc47f3398e5334fbfc8
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2023-05-08 07:44:26 -0300

    Add files via upload

---
+++ Clase8/src/AspectoRegistroTiempo.java
@@ -0,0 +1,15 @@
+public class AspectoRegistroTiempo 
+{
+/*@Around: 
+El proposito principal del Around es interceptar la ejecución
+de un metodo objetivo, realizar alguna accion antes y/o despues
+de la ejecución del metodo, y finalmente, decidir si se debe continuar
+o detener la ejecución del metodo objetivo.	
+En algunos casos el metodo puede necesitar devolver valores especificos.
+Por ejemplo, si el objetivo es un metodo que devuelve un valor, y
+se desea REEMPLAZAR ese valor, entonces el metodo debera devolver
+el nuevo valor modificado. Para hacer esto, el metodo "Around"
+puede llamar al metodo proceed() en el punto adecuado de la ejecución.
+capturar el resultado original del metodo objetivo y modificarlo. 	
+*/
+}
