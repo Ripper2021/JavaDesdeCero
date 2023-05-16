2121ffb3c8b544c50aa909b7232f8c3899872fc7
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2023-05-16 06:09:43 -0300

    Add files via upload

---
+++ Clase12/Clase12-MVCComponentes/src/Vista.java
@@ -0,0 +1,11 @@
+
+public class Vista 
+{
+	public void mostrarUsuario( String nombre, String correo)
+	{
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Correo: " + correo);
+	}
+	
+	
+}
---
+++ Clase12/Clase12/src/Usuario.java
@@ -0,0 +1,19 @@
+
+public class Usuario 
+{
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	private String correo;
+
+}
