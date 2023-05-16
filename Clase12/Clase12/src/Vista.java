6467173aac94c6085ef755d1637241d068b37c0a
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2023-05-16 06:10:34 -0300

    Delete Clase12 directory

--- Clase12/Clase12/src/Vista.java
+++
@@ -1,8 +0,0 @@
-public class Vista 
-{
-	public void mostrarIndex(Usuario usuario)
-	{
-		System.out.println("Bienvenido " + usuario.getNombre());
-		System.out.println("Correo electronico: " + usuario.getCorreo());
-	}
-}
